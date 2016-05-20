2f12ef6fcf2e2433943119c52939d8a6ebb6e1a8
Viktor <levik04122008@yandex.ru>
2016-05-20 16:03:03 +0300

    menu

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -67,6 +67,7 @@
 						<a href="http://fb.com/theme20" class="bottomtip" title="Like us on Facebook" target="_blank"><i class="icon-facebook"></i></a>
 						<a href="https://plus.google.com/u/0/104807493509867599773" class="bottomtip" title="GooglePlus" target="_blank"><i class="icon-google-plus"></i></a>
 						<a href="http://instagram.com/" class="bottomtip" title="instagram" target="_blank"><i class="icon-instagram"></i></a>
+						<a href="http://instagram.com/" class="bottomtip" title="instagram" target="_blank"><i class="icon-instagram"></i></a>
 						<a href="http://dribbble.com/behzadg" class="bottomtip" title="Dribbble" target="_blank"><i class="icon-dribbble"></i></a>
 						<a href="https://soundcloud.com/behzad-gh" class="bottomtip" title="Soundcloud" target="_blank"><i class="icon-cloud"></i></a>
 						<a href="http://reverbnation.com/" class="bottomtip" target="_blank" title="Reverbnation"><i class="icon-star"></i></a>
@@ -92,102 +93,102 @@
 
 					<nav>
 						<ul class="sf-menu">
-							<li class="current selectedLava"><a href="index.html">Home<span class="sub">start here</span></a>
+							<li class="current selectedLava"><a href="index.html">Главная<span class="sub">Начало здесь</span></a>
 								<ul>
-									<li><a href="index_revolution.html">Home 2 - Revolution</a></li>
-									<li class="current"><a href="index_modern.html">Home 3 - Modern</a></li>
-									<li><a href="index_footer.html">Home with Footer</a></li>
-									<li><a href="index_loading.html">Home with Loading</a></li>
+<!--									<li><a href="index_revolution.html">Home 2 - Revolution</a></li>-->
+<!--									<li class="current"><a href="index_modern.html">Home 3 - Modern</a></li>-->
+<!--									<li><a href="index_footer.html">Home with Footer</a></li>-->
+<!--									<li><a href="index_loading.html">Home with Loading</a></li>-->
 								</ul>
 							</li>
-							<li><a href="index.html">Features<span class="sub">remix all features</span></a>
+							<li><a href="index.html">Обо мне<span class="sub">и не только</span></a>
 								<ul>
-									<li><a href="#">Shortcodes</a>
-										<ul>
-											<li><a href="shortcode_accordion.html">Accordion</a></li>
-											<li><a href="shortcode_buttons.html">Buttons</a></li>
-											<li><a href="#">Typography</a>
-												<ul>
-													<li><a href="shortcode_columns.html">Grid Columns</a></li>
-													<li><a href="shortcode_quote.html">Quote</a></li>
-													<li><a href="shortcode_list_styles.html">List Styles</a></li>
-													<li><a href="shortcode_googlefonts.html">Google Fonts</a></li>
-													<li><a href="shortcode_dropcaps.html">Dropcaps</a></li>
-													<li><a href="shortcode_divider_lines.html">Divider Lines</a></li>
-													<li><a href="shortcode_highlights.html">Highlights</a></li>
-												</ul>
-											</li>
-											<li><a href="shortcode_tables.html">Tables</a></li>
-											<li><a href="shortcode_lightbox.html">Lightbox</a></li>
-											<li><a href="shortcode_googlemap.html">Google Map</a></li>
-											<li><a href="shortcode_video.html">Video</a></li>
-											<li><a href="shortcode_social_icons.html">Retina Social Icons</a></li>
-											<li><a href="shortcode_tab.html">Tab</a></li>
-											<li><a href="shortcode_toggle.html">Toggle</a></li>
-											<li><a href="shortcode_message_box.html">Message box</a></li>
-										</ul>
+									<li><a href="#">Биография</a>
+<!--										<ul>-->
+<!--											<li><a href="shortcode_accordion.html">Accordion</a></li>-->
+<!--											<li><a href="shortcode_buttons.html">Buttons</a></li>-->
+<!--											<li><a href="#">Typography</a>-->
+<!--												<ul>-->
+<!--													<li><a href="shortcode_columns.html">Grid Columns</a></li>-->
+<!--													<li><a href="shortcode_quote.html">Quote</a></li>-->
+<!--													<li><a href="shortcode_list_styles.html">List Styles</a></li>-->
+<!--													<li><a href="shortcode_googlefonts.html">Google Fonts</a></li>-->
+<!--													<li><a href="shortcode_dropcaps.html">Dropcaps</a></li>-->
+<!--													<li><a href="shortcode_divider_lines.html">Divider Lines</a></li>-->
+<!--													<li><a href="shortcode_highlights.html">Highlights</a></li>-->
+<!--												</ul>-->
+<!--											</li>-->
+<!--											<li><a href="shortcode_tables.html">Tables</a></li>-->
+<!--											<li><a href="shortcode_lightbox.html">Lightbox</a></li>-->
+<!--											<li><a href="shortcode_googlemap.html">Google Map</a></li>-->
+<!--											<li><a href="shortcode_video.html">Video</a></li>-->
+<!--											<li><a href="shortcode_social_icons.html">Retina Social Icons</a></li>-->
+<!--											<li><a href="shortcode_tab.html">Tab</a></li>-->
+<!--											<li><a href="shortcode_toggle.html">Toggle</a></li>-->
+<!--											<li><a href="shortcode_message_box.html">Message box</a></li>-->
+<!--										</ul>-->
 									</li>
-									<li><a href="shop.html">Shop</a>
-										<ul>
-											<li><a href="shop.html">Shop Page</a></li>
-											<li><a href="shop_product.html">Shop Product</a></li>
-											<li><a href="shop_cart.html">Shopping Cart</a></li>
-											<li><a href="shop_checkout.html">Shop Checkout</a></li>
-										</ul>
+									<li><a href="shop.html">Достижения и награды</a>
+<!--										<ul>-->
+<!--											<li><a href="shop.html">Shop Page</a></li>-->
+<!--											<li><a href="shop_product.html">Shop Product</a></li>-->
+<!--											<li><a href="shop_cart.html">Shopping Cart</a></li>-->
+<!--											<li><a href="shop_checkout.html">Shop Checkout</a></li>-->
+<!--										</ul>-->
 									</li>
-									<li><a href="under_construction.html">Under Construction</a></li>
-									<li><a href="icons.html"><i class="icon-microphone mi"></i>660 Retina Icons</a></li>
-									<li><a href="#"><i class="icon-caret-right mi"></i>3rd Level</a>
-										<ul>
-											<li><a href="#">1st Menu</a></li>
-											<li><a href="#">2nd Menu</a></li>
-											<li><a href="#">Another Sub</a>
-												<ul>
-													<li><a href="#">1st Menu</a></li>
-													<li><a href="#">2nd Menu</a></li>
-												</ul>
-											</li>
-										</ul>
-									</li>
+									<li><a href="under_construction.html">Что пишут СМИ</a></li>
+<!--									<li><a href="icons.html"><i class="icon-microphone mi"></i>660 Retina Icons</a></li>-->
+<!--									<li><a href="#"><i class="icon-caret-right mi"></i>3rd Level</a>-->
+<!--										<ul>-->
+<!--											<li><a href="#">1st Menu</a></li>-->
+<!--											<li><a href="#">2nd Menu</a></li>-->
+<!--											<li><a href="#">Another Sub</a>-->
+<!--												<ul>-->
+<!--													<li><a href="#">1st Menu</a></li>-->
+<!--													<li><a href="#">2nd Menu</a></li>-->
+<!--												</ul>-->
+<!--											</li>-->
+<!--										</ul>-->
+<!--									</li>-->
 								</ul>
 							</li>
-							<li><a href="mp3s.html">MP3<span class="sub">full archive</span></a></li>
-							<li><a href="videos.html">Video<span class="sub">latest clips</span></a></li>
-							<li><a href="gallery4.html">Gallery<span class="sub">Photo Gallery</span></a>
+							<li><a href="mp3s.html">Музыка<span class="sub">полный архив</span></a></li>
+							<li><a href="videos.html">Видео<span class="sub">все клипы</span></a></li>
+							<li><a href="gallery4.html">Фото<span class="sub">галерея</span></a>
 								<ul>
-									<li><a href="gallery4.html">Gallery 4 Column</a></li>
-									<li><a href="gallery3.html">Gallery 3 Column</a></li>
-									<li><a href="gallery2.html">Gallery 2 Column</a></li>
+<!--									<li><a href="gallery4.html">Gallery 4 Column</a></li>-->
+<!--									<li><a href="gallery3.html">Gallery 3 Column</a></li>-->
+<!--									<li><a href="gallery2.html">Gallery 2 Column</a></li>-->
 								</ul>
 							</li>
-							<li><a href="#">Pages<span class="sub">more templates</span></a>
+							<li><a href="#">Календарь<span class="sub">событий</span></a>
 								<ul>
-									<li><a href="rtl.html">RTL Support</a></li>
-									<li><a href="events.html">Events</a></li>
-									<li><a href="mp3_single_wide.html">MP3 Single Wide</a></li>
-									<li><a href="mp3_single_half.html">MP3 Single Half</a></li>
-									<li><a href="video_single_wide.html">Video Single Wide</a></li>
-									<li><a href="video_single_half.html">Video Single Half</a></li>
-									<li><a href="fullwide.html">Fullwide Page</a></li>
-									<li><a href="support_forum.html">Support Forum</a></li>
-									<li><a href="contact.html">Contact</a></li>
-									<li><a href="contact2.html">Contact 2</a></li>
-									<li><a href="search.html">Search Result</a></li>
-									<li><a href="404.html">Custom 404</a></li>
-									<li><a href="archive.html">Archive</a></li>
-									<li><a href="news_single.html">News Single</a></li>
+<!--									<li><a href="rtl.html">RTL Support</a></li>-->
+<!--									<li><a href="events.html">Events</a></li>-->
+<!--									<li><a href="mp3_single_wide.html">MP3 Single Wide</a></li>-->
+<!--									<li><a href="mp3_single_half.html">MP3 Single Half</a></li>-->
+<!--									<li><a href="video_single_wide.html">Video Single Wide</a></li>-->
+<!--									<li><a href="video_single_half.html">Video Single Half</a></li>-->
+<!--									<li><a href="fullwide.html">Fullwide Page</a></li>-->
+<!--									<li><a href="support_forum.html">Support Forum</a></li>-->
+<!--									<li><a href="contact.html">Contact</a></li>-->
+<!--									<li><a href="contact2.html">Contact 2</a></li>-->
+<!--									<li><a href="search.html">Search Result</a></li>-->
+<!--									<li><a href="404.html">Custom 404</a></li>-->
+<!--									<li><a href="archive.html">Archive</a></li>-->
+<!--									<li><a href="news_single.html">News Single</a></li>-->
 								</ul>
 							</li>
-							<li><a href="blog-right-sidebar.html">Blog<span class="sub">latest news</span></a>
+							<li><a href="blog-right-sidebar.html">Контакты<span class="sub">Связь со мной</span></a>
 								<ul>
-									<li><a href="blog-right-sidebar.html">Blog</a></li>
-									<li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
-									<li><a href="blog-both-sidebar.html">Blog Both Sidebar</a></li>
-									<li><a href="blog-without-sidebar.html">Blog Without Sidebar</a></li>
-									<li><a href="blog-single_wide.html">Blog Single Wide</a></li>
-									<li><a href="blog-single_half.html">Blog Single Half</a></li>
-									<li><a href="blog-single_facebook_comment.html">With Facebook Comment</a></li>
-									<li><a href="blog-single_comment_form.html">With Comment Form</a></li>
+<!--									<li><a href="blog-right-sidebar.html">Blog</a></li>-->
+<!--									<li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>-->
+<!--									<li><a href="blog-both-sidebar.html">Blog Both Sidebar</a></li>-->
+<!--									<li><a href="blog-without-sidebar.html">Blog Without Sidebar</a></li>-->
+<!--									<li><a href="blog-single_wide.html">Blog Single Wide</a></li>-->
+<!--									<li><a href="blog-single_half.html">Blog Single Half</a></li>-->
+<!--									<li><a href="blog-single_facebook_comment.html">With Facebook Comment</a></li>-->
+<!--									<li><a href="blog-single_comment_form.html">With Comment Form</a></li>-->
 								</ul>
 							</li>
 						</ul><!-- end menu -->
@@ -392,7 +393,7 @@
 									</li>
 									<li>
 										<a href="video_single_wide.html">
-											<iframe width="230" height="160" src="https://www.youtube.com/embed/FiXJUziXFVw" frameborder="0" allowfullscreen></iframe>
+											<iframe width="230" height="160" src="https://www.youtube.com/embed/K2LVoHDC1KU" frameborder="0" allowfullscreen></iframe>
 											<span><strong>Maya Rossa</strong>Я вернусь</span>
 										</a>
 									</li>
@@ -488,7 +489,7 @@
 					</div><!-- def block widget events -->
 
 					<div class="def-block widget animtt" data-gen="fadeUp" style="opacity:0;">
-						<h4> Это я в твиттере </h4><span class="liner"></span>
+						<h4> Это я в instagram </h4><span class="liner"></span>
 						<div class="widget-content">
 							<div class="tweet"></div>
 						</div><!-- widget content -->
@@ -550,7 +551,7 @@
 		<footer id="footer">
 			<div class="footer-last">
 				<div class="row clearfix">
-					<span class="copyright">© 2016 by <a href="http://mayarossa.ru/">mayarossa.ru</a>. All Rights Reserved. Powered by <a href="http://themeforest.net/user/behzadg?ref=behzadg">Themeforest</a>.</span>
+					<span class="copyright">При использовании информации с сайта, ссылка на сайт ОБЯЗАТЕЛЬНА! © 2016 by <a href="http://mayarossa.ru/">mayarossa.ru</a>.</span>
 					<div id="toTop"><i class="icon-angle-up"></i></div><!-- Back to top -->
 					<div class="foot-menu">
 						<ul>
